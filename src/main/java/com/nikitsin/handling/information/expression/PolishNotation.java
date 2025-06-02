172162ac709becea7f0a72a6de3cb17af2a3fbab
nikitinvitya <149611289+nikitinvitya@users.noreply.github.com>
2025-06-02 16:03:11 +0300

    Add files via upload

--- src/main/java/com/nikitsin/handling/information/expression/PolishNotation.java
+++ src/main/java/com/nikitsin/handling/information/expression/PolishNotation.java
@@ -13,36 +13,48 @@ public class PolishNotation {
         List<MathExpression> expressions = new ArrayList<>();
 
         Arrays.asList(polishNotation.split(SPLIT_REGEX)).forEach( token -> {
-            if (token.equals(AND_SIGN)) {
-                expressions.add(res -> res.push(res.pop() & res.pop()));
-            } else if (token.equals(LEFT_SHIFT_SIGN)) {
-                expressions.add(res -> {
-                    int right = res.pop();
-                    int left = res.pop();
-                    res.push(left << right);
-                });
-            } else if (token.equals(RIGHT_SHIFT_SIGN)) {
-                expressions.add(res -> {
-                    int right = res.pop();
-                    int left = res.pop();
-                    res.push(left >> right);
-                });
-            } else if (token.equals(TILDE_SIGN)) {
-                expressions.add(res -> res.push(~res.pop()));
-            } else if (token.equals(OR_SIGN)) {
-                expressions.add(res -> res.push(res.pop() | res.pop()));
-            } else if (token.equals(XOR_SIGN)) {
-                expressions.add(res -> res.push(res.pop() ^ res.pop()));
-            } else if (token.equals(PLUS_SIGN)) {
-                expressions.add(res -> res.push(res.pop() + res.pop()));
-            } else if (token.equals(MINUS_SIGN)) {
-                expressions.add(res -> res.push(-res.pop() + res.pop()));
-            } else if (token.equals(MULTIPLY_SIGN)) {
-                expressions.add(res -> res.push(res.pop() * res.pop()));
-            } else if (token.equals(DIVISION_SIGN)) {
-                expressions.add(res -> res.push(1 / res.pop() * res.pop()));
-            } else {
-                expressions.add(res -> res.push(Integer.parseInt(token)));
+            switch (token) {
+                case AND_SIGN:
+                    expressions.add(res -> res.push(res.pop() & res.pop()));
+                    break;
+                case LEFT_SHIFT_SIGN:
+                    expressions.add(res -> {
+                        int right = res.pop();
+                        int left = res.pop();
+                        res.push(left << right);
+                    });
+                    break;
+                case RIGHT_SHIFT_SIGN:
+                    expressions.add(res -> {
+                        int right = res.pop();
+                        int left = res.pop();
+                        res.push(left >> right);
+                    });
+                    break;
+                case TILDE_SIGN:
+                    expressions.add(res -> res.push(~res.pop()));
+                    break;
+                case OR_SIGN:
+                    expressions.add(res -> res.push(res.pop() | res.pop()));
+                    break;
+                case XOR_SIGN:
+                    expressions.add(res -> res.push(res.pop() ^ res.pop()));
+                    break;
+                case PLUS_SIGN:
+                    expressions.add(res -> res.push(res.pop() + res.pop()));
+                    break;
+                case MINUS_SIGN:
+                    expressions.add(res -> res.push(-res.pop() + res.pop()));
+                    break;
+                case MULTIPLY_SIGN:
+                    expressions.add(res -> res.push(res.pop() * res.pop()));
+                    break;
+                case DIVISION_SIGN:
+                    expressions.add(res -> res.push(1 / res.pop() * res.pop()));
+                    break;
+                default:
+                    expressions.add(res -> res.push(Integer.parseInt(token)));
+                    break;
             }
 
         });
